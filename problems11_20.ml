0d0b41c8af875ec0c2a2e81a748ec5cf1b865da1
Clint Newsom <hcnewsom@gmail.com>
2013-03-20 16:42:33 -0400

    new solutions to 11 and 12

--- problems11_20.ml
+++ problems11_20.ml
@@ -1,18 +1,50 @@
-open Problems1_10
+let pack_santi lst =
+    let rec f acc acc2 y lst =
+        match lst with
+        | [] -> List.rev (acc2 :: acc)
+        | x :: xs when x = y -> f acc (x::acc2) y xs
+        | x :: xs            -> f (acc2::acc) [x] x xs
+    in match lst with
+    | [] -> []
+    | x :: xs -> f [] [x] x xs;;
 
+(* Problem 11 *)
 type 'a rle =
     | One of 'a
     | Many of (int * 'a);;
 
-(* Problem 11 *)
-let ecnode lst = 
-  let p_list  = pack lst;
-    l_list = List.map p_list ~f:length
-    h_list = List.map list ~f:hd
-    z_list = zip h_list l_list
+let encode lst = 
+  let aux acc z =
+    match z with
+      | (1,x) -> (One x)::acc
+      | (i,x) -> (Many (i,x))::acc
+  and p_list  = pack_santi lst 
+  in
+    let l_list = List.map List.length p_list
+    and h_list = List.map List.hd p_list 
+    in
+      let z_list = List.combine l_list h_list 
+      in
+        List.rev(List.fold_left aux [] z_list);;
+
+(* Problem 12 *)
+
+(* Appending lists over and over is quadratic time. Would be better if
+   we rewrote times to accept an accumulator which is passed to the next 
+   outer list item. *)
+let rec times i x = 
+  if i > 0 
+  then x::(times(i-1) x)
+  else []
+;;
+
+let decode lst = 
+  let rec decode_item acc item = 
+    match item with
+      | One x -> x::acc
+      | Many(i,x) -> times i x @ acc
   in 
-  match z_list with 
-      [] -> []
-    | (1, x) -> One x
-    | (i, x) -> Many i x
-  ;;
+    List.rev(List.fold_left decode_item [] lst)
+
+
+(* Problem 13 *) 
