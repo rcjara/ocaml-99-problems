6d0fa371b88e627c483921235abeecadd2a0223e
Dan Luu <danluu@gmail.com>
2013-03-22 00:04:59 -0400

    Add extra test

--- test5.ml
+++ test5.ml
@@ -5,5 +5,6 @@ let () =
   print_string "Problem Forty-nine... ";
   assert(gray(1) = ["0";"1"]);
   assert(gray(2) = ["00";"01";"11";"10"]);
+  assert(gray(3) = ["000";"001";"011";"010";"110";"111";"101";"100"]);
   print_string "PASSED\n";
 ;;
