d139945c4e2cbd68a0b6ccdd53c447259d7077c2
Dan Luu <danluu@gmail.com>
2013-03-21 23:57:44 -0400

    Problem 49

---
+++ test5.ml
@@ -0,0 +1,9 @@
+open Problems41_50
+
+
+let () = 
+  print_string "Problem Forty-nine... ";
+  assert(gray(1) = ["0";"1"]);
+  assert(gray(2) = ["00";"01";"11";"10"]);
+  print_string "PASSED\n";
+;;
